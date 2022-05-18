0d689da5432d76261025b1537849ce2cbb89bc67
Rubén Saiz <rsaizhdam1@gmail.com>
2022-05-19 01:14:34 +0200

    Bad smell - Variable finJuego sustituible directamente por la condición dada para que el juego termine

--- src/AppJuego.java
+++ src/AppJuego.java
@@ -6,24 +6,15 @@ public class AppJuego {
         Jugador p1 = new Jugador();
         Jugador p2 = new Jugador();
         Juego juego = new Juego(p1, p2);
-        boolean finJuego = false;
         int rondasJugadas = 0;    // Número de rondas jugadas
-        while (!finJuego) {
-
-            System.out.println("***** Ronda: " + rondasJugadas+" *********************\n");
-            System.out.println("Numero de empates: "+ juego.getEmpates() + "\n");
+        while ((p1.éxitos < 3) || (p2.éxitos < 3)) {
+            System.out.println("***** Ronda: " + rondasJugadas + " *********************\n");
+            System.out.println("Numero de empates: " + juego.getEmpates() + "\n");
             juego.jugar();
-
-
-            if((p1.éxitos>=3)||(p2.éxitos>=3))
-            {
-                finJuego=true;
-                System.out.println("FIN DEL JUEGO!!");
-            } else {
-                rondasJugadas++;
-            }
+            rondasJugadas++;
             System.out.println();
         }
-
+        System.out.println("FIN DEL JUEGO!!");
     }
 }
+

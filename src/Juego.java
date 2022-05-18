f0b2813a4e2b554fb679c3a119ebe86c03b2ae62
Rubén Saiz <rsaizhdam1@gmail.com>
2022-05-19 00:41:54 +0200

    Convencion escritura - Cada clase en su fichero independiente

--- src/Juego.java
+++ src/Juego.java
@@ -74,39 +74,3 @@ public class Juego
         } while(finJuego!=true);
     }
 }
-/**
- *
- */
-class Jugador{
-   
-    /**
-     * Escoge piedra, papel o tijera al azar
-     */
-    public String opcion_al_azar()
-    {
-        String opcion="";
-        Integer c = (int)(Math.random()*3);
-        switch(c){
-            case 0:
-            	opcion=("piedra");
-                break;
-            case 1:
-            	opcion=("papel");
-                break;
-            case 2:
-            	opcion=("tijeras");
-        }
-        return opcion;
-    }
-    public void setÉxitos() 
-    {
-        éxitos++;
-    }
-    public int getÉxitos() 
-    {
-        return(éxitos);
-    }
-    
-    int éxitos;      // número de partidas ganadas
-    int winTotal;
-}

0b0b37fc1a9c135968c84717d2b79ae172b1bc09
Rubén Saiz <rsaizhdam1@gmail.com>
2022-05-19 00:37:53 +0200

    Buena practica - cambiadas clases wrapper por tipos primitivos

--- src/Juego.java
+++ src/Juego.java
@@ -12,10 +12,10 @@ public class Juego
         Jugador p1=new Jugador();
         Jugador p2=new Jugador();
         boolean finJuego=false;  
-        Integer rondasJugadas = 0;    // Número de rondas jugadas
-        Integer exitosJugador1=p1.éxitos;
-        Integer exitosJugador2=p2.éxitos;
-        Integer empates = 0;
+        int rondasJugadas = 0;    // Número de rondas jugadas
+        int exitosJugador1=p1.éxitos;
+        int exitosJugador2=p2.éxitos;
+        int empates = 0;
         String opcionJugador1, opcionJugador2;
         
         // Bucle de juego

84d7e3709474ebd9e941b9f5b5fbb9ec77cbcab8
Rubén Saiz <rsaizhdam1@gmail.com>
2022-05-19 01:21:11 +0200

    Buena práctica - Refactorizado switch clase Jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -14,14 +14,9 @@ class Jugador {
         String opcion = "";
         int c = (int) (Math.random() * 3);
         switch (c) {
-            case 0:
-                opcion = ("piedra");
-                break;
-            case 1:
-                opcion = ("papel");
-                break;
-            case 2:
-                opcion = ("tijeras");
+            case 0 -> opcion = ("piedra");
+            case 1 -> opcion = ("papel");
+            case 2 -> opcion = ("tijeras");
         }
         return opcion;
     }

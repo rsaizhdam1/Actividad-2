7fe54bf44c131997796ed9a009f2a741fc410fbe
Rubén Saiz <rsaizhdam1@gmail.com>
2022-05-19 01:27:35 +0200

    Buena práctica - Eliminadas variable local y atributo sin uso

--- src/Jugador.java
+++ src/Jugador.java
@@ -9,9 +9,7 @@ class Jugador {
      * Escoge piedra, papel o tijera al azar
      */
     private int exitos;      // número de partidas ganadas
-    private int winTotal;
     public String opcionAlAzar() {
-        String opcion = "";
         int c = (int) (Math.random() * 3);
         return switch (c) {
             case 0 -> "piedra";

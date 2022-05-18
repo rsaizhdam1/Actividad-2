a74a501ffd7d42b805ac0be4ba92807739b6e1ca
Rubén Saiz <rsaizhdam1@gmail.com>
2022-05-19 01:41:34 +0200

    Bad smells - duplicidad de codigo en Juego

--- src/Juego.java
+++ src/Juego.java
@@ -26,39 +26,33 @@ public class Juego {
         System.out.println("Jugador 1: " + opcionJugador1+"\t Jugador 1 - Partidas ganadas: " + p1.getExitos());
         String opcionJugador2 = p2.opcionAlAzar();
         System.out.println("Jugador 2: " + opcionJugador2+"\t Jugador 2 - Partidas ganadas: " + p2.getExitos());
-            if((opcionJugador1.equals("piedra"))&&(opcionJugador2.equals("papel")))
-            {
-                System.out.println("Jugador 2 GANA");
-                p2.setÉxitos();
-                
+            if(opcionJugador1.equals("piedra")) {
+               if (opcionJugador2.equals("papel")) {
+                   System.out.println("Jugador 2 GANA");
+                   p2.setExitos();
+                } else if (opcionJugador2.equals("tijera")) {
+                   System.out.println("Jugador 1 GANA");
+                   p1.setExitos();
+               }
+            } else if(opcionJugador1.equals("papel")) {
+                if (opcionJugador2.equals("piedra")) {
+                    System.out.println("Jugador 1 GANA");
+                    p1.setExitos();
+                } else if (opcionJugador2.equals("tijera")){
+                    System.out.println("Jugador 2 GANA");
+                    p2.setExitos();
+                }
+            } else {
+                if (opcionJugador2.equals("piedra")){
+                    System.out.println("Jugador 2 GANA");
+                    p2.setExitos();
+                } else if(opcionJugador2.equals("papel")){
+                    System.out.println("Jugador 1 GANA");
+                    p1.setExitos();
+                }
             }
-            else if((opcionJugador1.equals("papel"))&&(opcionJugador2.equals("piedra")))
-            {
-                System.out.println("Jugador 1 GANA");
-                p1.setÉxitos();
-            }
-            else if((opcionJugador1.equals("piedra"))&&(opcionJugador2.equals("tijeras")))
-            {
-                System.out.println("Jugador 1 GANA");
-                p1.setÉxitos();
-            }
-            else if((opcionJugador1.equals("tijeras"))&&(opcionJugador2.equals("piedra")))
-            {
-                System.out.println("Jugador 2 GANA");
-                p2.setÉxitos();
-            }
-            else if((opcionJugador1.equals("tijeras"))&&(opcionJugador2.equals("papel")))
-            {
-                System.out.println("Jugador 1 GANA");
-                p1.setÉxitos();
-            }
-            else if((opcionJugador1.equals("papel"))&&(opcionJugador2.equals("tijeras")))
-            {
-                System.out.println("Jugador 2 GANA");
-                p2.setÉxitos();
-            }
-            if(opcionJugador1==opcionJugador2)
-            {
+
+            if(opcionJugador1==opcionJugador2) {
             	empates++;
                 System.out.println("\n\t\t\t Empate \n");
             }
